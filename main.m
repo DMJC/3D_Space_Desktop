86da3726b1e3ac1d08c43878e2fa07cd7ae656c7
DMJC <jcarthew@gmail.com>
2026-05-06 23:54:05 +1000

    Add POF load debug logs and ESC key quit support

--- main.m
+++ main.m
@@ -80,20 +80,56 @@ V(h,-h,-h);V(h,h,-h);V(h,h,h);V(h,-h,h); V(-h,-h,-h);V(-h,-h,h);V(-h,h,h);V(-h,h
 #undef V
 glEnd(); }
 
+
+static BOOL LoadPOFDetail0(NSString *pofPath, NSString **errorMessage) {
+    BOOL isDir = NO;
+    if (![[NSFileManager defaultManager] fileExistsAtPath:pofPath isDirectory:&isDir] || isDir) {
+        if (errorMessage) *errorMessage = @"POF file not found";
+        return NO;
+    }
+    NSData *data = [NSData dataWithContentsOfFile:pofPath];
+    if (!data || [data length] < 4) {
+        if (errorMessage) *errorMessage = @"POF file is unreadable or too small";
+        return NO;
+    }
+    const char *bytes = (const char *)[data bytes];
+    if (!(bytes[0]=='P' && bytes[1]=='S' && bytes[2]=='P' && bytes[3]=='O')) {
+        if (errorMessage) *errorMessage = @"POF header signature mismatch";
+        return NO;
+    }
+    if (errorMessage) *errorMessage = @"POF parsed header OK; detail0 mesh decode not implemented yet";
+    return NO;
+}
+
 @interface SpaceGLView : NSOpenGLView
 - (instancetype)initWithFrame:(NSRect)frame scene:(SceneDefinition *)scene;
 @end
-@implementation SpaceGLView { SceneDefinition *_scene; NSTimer *_timer; NSTimeInterval _last; float _elapsed; }
-- (instancetype)initWithFrame:(NSRect)frame scene:(SceneDefinition *)scene { NSOpenGLPixelFormatAttribute attrs[]={NSOpenGLPFADoubleBuffer,NSOpenGLPFAColorSize,24,NSOpenGLPFADepthSize,24,0}; if((self=[super initWithFrame:frame pixelFormat:[[NSOpenGLPixelFormat alloc] initWithAttributes:attrs]])){ _scene=scene; _last=[NSDate timeIntervalSinceReferenceDate]; _timer=[NSTimer scheduledTimerWithTimeInterval:1.0/60.0 target:self selector:@selector(onTick:) userInfo:nil repeats:YES]; } return self; }
+@implementation SpaceGLView { SceneDefinition *_scene; NSTimer *_timer; NSTimeInterval _last; float _elapsed; NSMutableSet *_loggedPOFs; }
+- (instancetype)initWithFrame:(NSRect)frame scene:(SceneDefinition *)scene { NSOpenGLPixelFormatAttribute attrs[]={NSOpenGLPFADoubleBuffer,NSOpenGLPFAColorSize,24,NSOpenGLPFADepthSize,24,0}; if((self=[super initWithFrame:frame pixelFormat:[[NSOpenGLPixelFormat alloc] initWithAttributes:attrs]])){ _scene=scene; _last=[NSDate timeIntervalSinceReferenceDate]; _timer=[NSTimer scheduledTimerWithTimeInterval:1.0/60.0 target:self selector:@selector(onTick:) userInfo:nil repeats:YES]; _loggedPOFs=[NSMutableSet set]; } return self; }
 - (void)onTick:(NSTimer*)t { (void)t; [self display]; }
 - (void)prepareOpenGL { [super prepareOpenGL]; glEnable(GL_DEPTH_TEST); }
 - (void)reshape { [super reshape]; NSRect b=[self bounds]; glViewport(0,0,b.size.width,b.size.height); glMatrixMode(GL_PROJECTION); glLoadIdentity(); float asp=b.size.height>0?b.size.width/b.size.height:1; float n=.1f,f=5000.f,top=tanf(DegToRad(30))*n,right=top*asp; glFrustum(-right,right,-top,top,n,f); }
 - (void)drawRect:(NSRect)r { (void)r; NSTimeInterval n=[NSDate timeIntervalSinceReferenceDate]; _elapsed += (float)(n-_last); _last=n; glClearColor(0.01,0.01,0.03,1); glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT); glMatrixMode(GL_MODELVIEW); glLoadIdentity(); glTranslatef(-_scene.cameraPosition.x,-_scene.cameraPosition.y,-_scene.cameraPosition.z);
     for(SceneModel *m in _scene.models){ Vec3 p=m.position; if(m.hasOvalPath&&m.ovalRadius>0){ float a=DegToRad(m.ovalAngleOffset+_elapsed*m.ovalSpeedDegPerSec); p.x+=cosf(a)*m.ovalRadius; p.z+=sinf(a)*m.ovalRadius; }
-        glPushMatrix(); glTranslatef(p.x,p.y,p.z); BOOL found=[[NSFileManager defaultManager] fileExistsAtPath:m.pofPath]; glColor3f(found?0.6:1.0,found?0.8:0.1,found?1.0:0.1); DrawCube(found?8.f:12.f); glPopMatrix();
+        glPushMatrix(); glTranslatef(p.x,p.y,p.z); NSString *pofError=nil; BOOL found=LoadPOFDetail0(m.pofPath,&pofError);
+        if(![_loggedPOFs containsObject:m.pofPath]){
+            if(!found){ NSLog(@"[POF] FAILED %@ -> %@", m.pofPath, pofError); }
+            else { NSLog(@"[POF] LOADED detail0 %@", m.pofPath); }
+            [_loggedPOFs addObject:m.pofPath];
+        }
+        glColor3f(found?0.6:1.0,found?0.8:0.1,found?1.0:0.1); DrawCube(found?8.f:12.f); glPopMatrix();
         if(_scene.showLoops && m.hasOvalPath && m.ovalRadius>0){ glColor3f(1,0,0); glLineWidth(3); glBegin(GL_LINE_LOOP); for(int i=0;i<96;i++){ float a=DegToRad(360.f*i/96.f); glVertex3f(m.position.x+cosf(a)*m.ovalRadius,m.position.y,m.position.z+sinf(a)*m.ovalRadius);} glEnd(); }
     }
     [[self openGLContext] flushBuffer]; }
+- (BOOL)acceptsFirstResponder { return YES; }
+- (void)keyDown:(NSEvent *)event {
+    NSString *chars = [event charactersIgnoringModifiers];
+    if ([chars length] > 0 && [chars characterAtIndex:0] == 27) {
+        [NSApp terminate:nil];
+        return;
+    }
+    [super keyDown:event];
+}
 @end
 
 int main(int argc,char **argv){ NSAutoreleasePool *pool=[NSAutoreleasePool new]; NSString *scenePath=@"scene.txt"; if(argc>1) scenePath=[NSString stringWithUTF8String:argv[1]]; NSError *err=nil; SceneDefinition *scene=[SceneParser parseSceneAtPath:scenePath error:&err]; if(!scene){ fprintf(stderr,"scene load failed: %s\n",[[err description] UTF8String]); return 1; } [NSApplication sharedApplication]; NSWindow *w=[[NSWindow alloc] initWithContentRect:NSMakeRect(0,0,1280,720) styleMask:(NSTitledWindowMask|NSClosableWindowMask|NSResizableWindowMask) backing:NSBackingStoreBuffered defer:NO]; [w setTitle:@"3D Scene Viewer (GNUstep)"]; [w setContentView:[[SpaceGLView alloc] initWithFrame:NSMakeRect(0,0,1280,720) scene:scene]]; [w makeKeyAndOrderFront:nil]; [NSApp run]; [pool drain]; return 0; }
